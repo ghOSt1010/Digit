da0302af337cf0fddc512b549b535a88b4af1872
ghOSt1010 <43892935+ghOSt1010@users.noreply.github.com>
2018-11-17 11:45:17 +0100

    User Story 1,2,3 - solved

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,43 +1,16 @@
 package com.company;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
     public static void main(String[] args) {
         Digit digit = new Digit();
-
-        String[] v0 = {" _ ", "| |", "|_|"};
-        String[] v1 = {"   ", "  |", "  |",};
-        String[] v2 = {" _ ", " _|", "|_ ",};
-        String[] v3 = {" _ ", " _|", " _|",};
-        String[] v4 = {"   ", "|_|", "  |",};
-        String[] v5 = {" _ ", "|_ ", " _|",};
-        String[] v6 = {" _ ", "|_ ", "|_|",};
-        String[] v7 = {" _ ", "  |", "  |",};
-        String[] v8 = {" _ ", "|_|", "|_|",};
-        String[] v9 = {" _ ", "|_|", " _|",};
-
-        String testValue = "    _  _     _  _  _  _  _\n" +
-                           "  | _| _||_||_ |_   ||_||_|\n" +
-                           "  ||_  _|  | _||_|  ||_| _|";
-
-        String S = " _  _  _  _  _  _  _  _  _\n" +
-                   "|_||_||_||_||_||_||_||_||_|\n" +
-                   "|_||_||_||_||_||_||_||_||_|";
-
-
-        System.out.println(digit.convertStringToAccountNumberInt(testValue));
-        //digit.convertInputToArrays(S);
-        /*
-        System.out.println(digit.convert(v0));
-        System.out.println(digit.convert(v1));
-        System.out.println(digit.convert(v2));
-        System.out.println(digit.convert(v3));
-        System.out.println(digit.convert(v4));
-        System.out.println(digit.convert(v5));
-        System.out.println(digit.convert(v6));
-        System.out.println(digit.convert(v7));
-        System.out.println(digit.convert(v8));
-        System.out.println(digit.convert(v9));
-        */
+       String GetPath = new File("").getAbsolutePath() + "/src/Inputs/Inputfile.txt";
+       Path file = Paths.get(GetPath);
+       AccountReader reader = new AccountReader(file);
+       System.out.println(reader.GetAllAccounts());
     }
 }
