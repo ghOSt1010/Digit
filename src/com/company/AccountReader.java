5fe0d284f8219adafdbbb1da63dec8334f4215a3
ghOSt1010 <43892935+ghOSt1010@users.noreply.github.com>
2018-11-17 11:26:01 +0100

    User Story 1,2,3 - solved

---
+++ src/com/company/AccountReader.java
@@ -0,0 +1,71 @@
+package com.company;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.nio.file.Files.readAllLines;
+
+public class AccountReader {
+   private Path pathFile;
+   private List<String> readFile;
+
+   public AccountReader(Path pathFile) {
+      this.pathFile = pathFile;
+      ReadFile();
+   }
+
+   public AccountReader ReadFile() {
+      try {
+         this.readFile = readAllLines(pathFile);
+      } catch (Exception e) {
+         System.out.println("nie udalo sie wczytać pliku");
+      }
+      return this;
+   }
+
+   public List<String[]> IsolateAccounts() {
+      List<String[]> listAccounts = new ArrayList<>();
+
+      for (int i = 0; i < this.readFile.size() / 4; i++) {
+         listAccounts.add(
+            new String[]{
+               this.readFile.get(i * 4),
+               this.readFile.get((i * 4) + 1),
+               this.readFile.get((i * 4) + 2),
+            }
+         );
+      }
+      return listAccounts;
+   }
+
+   public List<String> GetAllAccounts() {
+
+      List<String[]> listAccounts = IsolateAccounts();
+      List<String> result = new ArrayList<>();
+
+      for (String[] account : listAccounts) {
+         String transformedAccount = new Digit().convertStringToAccountNumber(account);
+         if (CheckErrorAccount(transformedAccount)) {
+            result.add(transformedAccount + "ILL");
+         } else if (!CheckAccountSumControl(transformedAccount)) {
+            result.add(transformedAccount + " ERR");
+         } else {
+            result.add(transformedAccount);
+         }
+      }
+      return result;
+   }
+
+   public boolean CheckErrorAccount(String account) {
+      return account.contains("?");
+   }
+
+   public boolean CheckAccountSumControl(String acount) {
+      int sum = 0;
+      for (int i = 1; i <= acount.length(); i++) {
+         sum += i * Integer.parseInt(acount.substring(i - 1, i));
+      }
+      return sum % 11 == 0;
+   }
+}
--- src/com/company/Digit.java
+++ src/com/company/Digit.java
@@ -2,32 +2,32 @@ package com.company;
 
 
 /*
-*
-*  First user story to be solved
+ *
+ *  First user story to be solved
  *  User Story 1 - Solved :)
-*
-*
-*/
+ *
+ *
+ */
 
 class Digit {
 
-    private String w0 = " _ ";
-    private String w1 = "   ";
-    private String w2 = "  |";
-    private String w3 = "| |";
-    private String w4 = "|_|";
-    private String w5 = " _|";
-    private String w6 = "|_ ";
+   private String w0 = " _ ";
+   private String w1 = "   ";
+   private String w2 = "  |";
+   private String w3 = "| |";
+   private String w4 = "|_|";
+   private String w5 = " _|";
+   private String w6 = "|_ ";
 
 
    //Convert input to account number as char array
-   public char[] convertStringToAccountNumberCharArray(String Input){
+   public char[] convertStringToAccountNumberCharArray(String[] Input) {
       char[] Result = this.convertStringToAccountNumber(Input).toCharArray();
       return Result;
    }
 
    //Convert input to account number as int array
-   public int convertStringToAccountNumberInt(String Input){
+   public int convertStringToAccountNumberInt(String[] Input) {
       int Result;
       String AccountNumber= this.convertStringToAccountNumber(Input);
       Result = Integer.parseInt(AccountNumber);
@@ -35,8 +35,8 @@ class Digit {
    }
 
    //Convert input to account Number as String
-   public String convertStringToAccountNumber(String Input){
-      String Result = new String();
+   public String convertStringToAccountNumber(String[] Input) {
+      String Result = "";
       for(int i = 0; i < 9;i++){
          Result += this.convertStringToNumber(Input,i);
       }
@@ -44,69 +44,51 @@ class Digit {
    }
 
    //Convert input to number by id. ID range 0-8
-   public String convertStringToNumber(String Input, int Digit_Place){
+   public String convertStringToNumber(String[] Input, int Digit_Place) {
       String Result;
-      //error handle digits are 0-9
-      int[] Start_IDs = {0,3,6,9,12,15,18,21,24};
 
-      StringBuilder _Input = new StringBuilder(Input);
-      char emptychar = ' ';
-
-      //removing problematic \n char <- no new line in char array
-      _Input.setCharAt(26,emptychar);
-      _Input.setCharAt(54,emptychar);
-
-      String line0 = _Input.toString().substring( 0,  27 );  //0-27 <- first line -1 as \n
-      String line1 = _Input.toString().substring( 27,  54 ); //27-55 <- second line -1 as \n
-      String line2 = _Input.toString().substring( 55,  82 ); //55-82 < third line
-      String[] Lines = {line0,line1,line2}; 
-
-
-      String[] Output = {Lines[0].substring(Start_IDs[Digit_Place],Start_IDs[Digit_Place]+3),
-                         Lines[1].substring(Start_IDs[Digit_Place],Start_IDs[Digit_Place]+3),
-                         Lines[2].substring(Start_IDs[Digit_Place],Start_IDs[Digit_Place]+3)};
+      String[] Output = {Input[0].substring(Digit_Place * 3, (Digit_Place * 3) + 3),
+         Input[1].substring(Digit_Place * 3, (Digit_Place * 3) + 3),
+         Input[2].substring(Digit_Place * 3, (Digit_Place * 3) + 3)};
 
       Result = this.convert(Output);
       return Result;
    }
 
    public String convert(String[] digit) {
-        if (digit.length != 3) {
-            return "?";
-        }
-        if (digit[0].equals(w1)) {
-            if (digit[1].equals(w2)){
-                if (digit[2].equals(w2))return "1";
-                else return "?";
-            } else if (digit[1].equals(w4)){
-                if (digit[2].equals(w2))return "4";
-                else return "?";
-            }
-        } else {
-            if (digit[1].equals(w2)) {
-                if (digit[2].equals(w2)) return "7";
-                else return "?";
-            } else if (digit[1].equals(w3)) {
-                if (digit[2].equals(w4)) return "0";
-                else return "?";
-            } else if (digit[1].equals(w4)) {
-                if (digit[2].equals(w4)) return "8";
-                else if (digit[2].equals(w5)) return "9";
-                else return "?";
-            } else if (digit[1].equals(w5)) {
-                if (digit[2].equals(w5)) return "3";
-                else if (digit[2].equals(w6)) return "2";
-                else return "?";
-            } else if (digit[1].equals(w6)) {
-                if (digit[2].equals(w5)) return "5";
-                else if (digit[2].equals(w4)) return "6";
-                else return "?";
-            }
-        }
-        return "?";
-    }
+      if (digit.length != 3) {
+         return "?";
+      }
+      if (digit[0].equals(w1)) {
+         if (digit[1].equals(w2)) {
+            if (digit[2].equals(w2)) return "1";
+            else return "?";
+         } else if (digit[1].equals(w4)) {
+            if (digit[2].equals(w2)) return "4";
+            else return "?";
+         }
+      } else {
+         if (digit[1].equals(w2)) {
+            if (digit[2].equals(w2)) return "7";
+            else return "?";
+         } else if (digit[1].equals(w3)) {
+            if (digit[2].equals(w4)) return "0";
+            else return "?";
+         } else if (digit[1].equals(w4)) {
+            if (digit[2].equals(w4)) return "8";
+            else if (digit[2].equals(w5)) return "9";
+            else return "?";
+         } else if (digit[1].equals(w5)) {
+            if (digit[2].equals(w5)) return "3";
+            else if (digit[2].equals(w6)) return "2";
+            else return "?";
+         } else if (digit[1].equals(w6)) {
+            if (digit[2].equals(w5)) return "5";
+            else if (digit[2].equals(w4)) return "6";
+            else return "?";
+         }
+      }
+      return "?";
+   }
 
 }
-
-
-
